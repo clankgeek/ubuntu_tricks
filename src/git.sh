938022195353fa8599c74ca8417867f912ea4162
Cédric Champetier <cedric.champetier@ubikasec.com>
2024-12-05 11:53:19 +0100

    fix: minor bug

--- src/git.sh
+++ src/git.sh
@@ -5,6 +5,8 @@ echo "Installing git"
 sudo apt install -f git tig vim
 git config --global core.editor "vim"
 git config --global core.autocrlf input
+git config --global pull.rebase true
+git config --global rebase.autoStash true
 tee -a ~/.bashrc << EOM
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
--- src/golang.sh
+++ src/golang.sh
@@ -5,10 +5,11 @@ echo "Installing golang"
 #sudo add-apt-repository ppa:longsleep/golang-backports
 #sudo apt update
 sudo apt install -f golang-go
-echo "" >> ~/.bashrc
 mkdir ~/go
-echo "export GOPATH=$HOME/go" >> ~/.bashrc
-echo "export PATH=\$PATH:\$GOPATH/bin" >> ~/.bashrc
 export GOPATH=$HOME/go
 export PATH=$PATH:$GOPATH/bin
+
+echo "" >> ~/.bashrc
+echo "export GOPATH=$HOME/go" >> ~/.bashrc
+echo "export PATH=\$PATH:\$GOPATH/bin" >> ~/.bashrc
 
