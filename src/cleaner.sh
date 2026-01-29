0334ef40e60c085f6b1c3c33db33edcb35ae5cc5
Cédric Champetier <cedric.champetier@ubikasec.com>
2026-01-29 19:25:28 +0100

    feat: proton-up qt dans gaming.sh

--- src/cleaner.sh
+++ src/cleaner.sh
@@ -6,7 +6,7 @@ sudo apt autoclean
 sudo apt --purge autoremove -y
 sudo apt purge ~c -y
 
-if [ -e /usr/lib/snapd ]; then
+if command -v snap &>/dev/null; then
     sudo snap set system refresh.retain=2
     LANG=C snap list --all | awk '/disabled/{print $1, $3}' | while read snapname revision; do sudo snap remove "$snapname" --revision="$revision"; done
     LANG=C snap saved|grep -Eo "^([0-9]+)" | while read snapid; do sudo snap forget "$snapid"; done
--- src/gaming.sh
+++ src/gaming.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
+install_flatpak() {
+	sudo apt install -y flatpak
+	sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+}
+
+install_protonup() {
+	echo "Installing ProtonUp-Qt (flatpak)"
+	install_flatpak
+	sudo flatpak install -y net.davidotek.pupgui2
+}
+
 install_steam() {
 	echo "Installing Steam snap version"
 	sudo apt install -y snapd
 	sudo snap install steam
 	sudo snap refresh gaming-graphics-core22 --channel kisak-fresh
 	sudo apt install -y steam-devices --no-install-recommends
-
-	echo ""
-	echo "Download ProtonUp-QT appimage from https://github.com/DavidoTek/ProtonUp-Qt/tags"
-	echo "Make it executable and run it to install additional Proton versions or other compatibility tools like GloriousEggroll's GE-Proton."
-	echo "To enable Proton-GE for all titles, go to Steam Settings > compatibilities and choose 'Proton-GE'."
-	echo "Enjoy gaming on Linux!"
-	echo ""
 }
 
 install_heroic() {
 	echo "Installing Heroic Games Launcher (flatpak)"
-	sudo apt install -y flatpak
-	sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	install_flatpak
 	sudo flatpak install -y com.heroicgameslauncher.hgl
 }
 
 install_lutris() {
 	echo "Installing Lutris (flatpak)"
-	sudo apt install -y flatpak
-	sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	install_flatpak
 	sudo flatpak install -y net.lutris.Lutris
 }
 
@@ -33,7 +35,8 @@ echo "What launcher do you want to install?"
 echo " 1 - Steam (snap)"
 echo " 2 - Heroic Games Launcher (flatpak)"
 echo " 3 - Lutris (flatpak)"
-echo " 4 - Steam + Heroic + Lutris"
+echo " 4 - ProtonUp-Qt (flatpak)"
+echo " 5 - Steam + Heroic + Lutris + ProtonUp-Qt"
 echo "What is your choice ?"
 
 read menu_choice
@@ -41,10 +44,12 @@ case "$menu_choice" in
 1) install_steam ;;
 2) install_heroic ;;
 3) install_lutris ;;
-4)
+4) install_protonup ;;
+5)
 	install_steam
 	install_heroic
 	install_lutris
+	install_protonup
 	;;
 *) echo "Nothing to do" ;;
 esac
