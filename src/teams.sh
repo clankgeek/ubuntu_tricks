cd6b1e2d3d7b8413eda483a1780ac09730179e77
Cédric Champetier <cedric.champetier@ubikasec.com>
2026-05-06 13:46:56 +0200

    feat: add teams for linux

---
+++ src/teams.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Installing and configuring Teams for linux
+
+echo "Installing Teams for linux"
+
+sudo mkdir -p /etc/apt/keyrings
+sudo wget -qO /etc/apt/keyrings/teams-for-linux.asc https://repo.teamsforlinux.de/teams-for-linux.asc
+sh -c 'echo "Types: deb\nURIs: https://repo.teamsforlinux.de/debian/\nSuites: stable\nComponents: main\nSigned-By: /etc/apt/keyrings/teams-for-linux.asc\nArchitectures: amd64" | sudo tee /etc/apt/sources.list.d/teams-for-linux-packages.sources'
+sudo apt update && sudo apt install teams-for-linux
